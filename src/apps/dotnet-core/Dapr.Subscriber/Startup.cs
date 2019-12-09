375a7de85fd162a3de07f7d09766574c7d413f85
James Eastham <j.eastham1993@gmail.com>
2019-12-09 23:27:48 +0000

    [FEATURE] Add dotnet core pub/sub samples

---
+++ src/apps/dotnet-core/Dapr.Subscriber/Startup.cs
@@ -0,0 +1,65 @@
+using System.Text.Json;
+using System.Threading.Tasks;
+using Dapr;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.IO;
+using System.Collections.Generic;
+
+
+namespace Dapr.Subscriber
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDaprClient();
+
+            services.AddSingleton(new JsonSerializerOptions()
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                PropertyNameCaseInsensitive = true,
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/dapr/subscribe", async context => {
+                    var subscribedTopics = new List<string>(){"topic1"};
+                    await context.Response.WriteAsync(JsonSerializer.Serialize(subscribedTopics));
+                });
+
+                endpoints.MapPost("/topic1", async context => {
+                    using (var streamReader = new StreamReader(context.Request.Body))
+                    {
+                        var json = await streamReader.ReadToEndAsync();
+
+                        System.Console.WriteLine(json);
+                    }
+                });
+            });
+        }
+    }
+}
---
+++ src/apps/dotnet-core/Dapr.WebApiSample/Controllers/MessagePublishController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Dapr.WebApiSample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MessagePublishController : ControllerBase
+    {
+        private readonly ILogger<MessagePublishController> _logger;
+        private readonly HttpClient _httpClient;
+
+        
+        public MessagePublishController(ILogger<MessagePublishController> logger)
+        {
+            _logger = logger;
+            this._httpClient = new HttpClient();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(string id)
+        {
+            await this._httpClient.PostAsync("http://localhost:53034/v1.0/publish/topic1", new StringContent("{ \"status\": \"completed\", \"id\": \"1\"}"));
+
+            return new OkResult();
+        }
+    }
+}
